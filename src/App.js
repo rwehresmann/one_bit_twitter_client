d8ddf4b3ab5f28a9d67b8e00ba5e722052bba8ed
rwehresmann <igoehresmann@gmail.com>
2018-09-02 19:21:24 -0300

    Install materialize
    Our css framework.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Carousel } from 'react-materialize';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Carousel images={[
+          'https://lorempixel.com/250/250/nature/1',
+          'https://lorempixel.com/250/250/nature/2',
+          'https://lorempixel.com/250/250/nature/3',
+          'https://lorempixel.com/250/250/nature/4',
+          'https://lorempixel.com/250/250/nature/5',
+        ]} />
       </div>
     );
   }
