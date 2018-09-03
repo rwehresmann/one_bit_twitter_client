896db8b9e4cd223140c87366a350963384c8fcd1
rwehresmann <igoehresmann@gmail.com>
2018-09-02 21:01:27 -0300

    Introduce styled-components
    https://www.styled-components.com/docs/basics

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,31 @@
 import React from "react";
 import { Navbar, NavItem, Row, Icon } from 'react-materialize';
 
+import styled from 'styled-components';
 import logo from '../../images/logo.png';
-import './index.css';
+
+const NavbarBlue = styled(Navbar)`
+  background-color: #75d3f4;`
+;
+
+const Logo = styled.img`
+  margin-top: 15px;`
+;
+
+const IconUser = styled(Icon)`
+  font-size: 40px !important;`
+;
 
 const Header = () => (
-  <Row>
-    <Navbar brand={<img src={logo} alt="OneBitTwitter" 
-    className="responsive-img col m6 center logo" />} right className="navbar">
+  <Row>  
+    <NavbarBlue brand={<Logo src={logo} className="responsive-img col m6 center" />} right>
       <NavItem href='/logout'>
         Logout
       </NavItem>
       <NavItem href='/user/x/edit'>
-          <Icon class="icon_user">account_circle</Icon>
+        <IconUser>account_circle</IconUser>
       </NavItem>
-    </Navbar>
+    </NavbarBlue>
   </Row>
 );
 
