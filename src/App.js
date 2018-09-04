916904071c14c2f962df91cb148d86844232ce42
rwehresmann <igoehresmann@gmail.com>
2018-09-04 00:28:01 -0300

    Introduce MainLayout
    Like the name says: our main layout of the site. It receives the page header and a container to display the content.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
 import ProfilePageContainer from './containers/ProfilePageContainer'
-import { Container } from 'react-materialize';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <Container>
-          <ProfilePageContainer/>
-        </Container>
-      </div>
+      <ProfilePageContainer/>
     );
   }
 }
