d13851d4cdbde6ea5753cd72daa4b998f55bbfa9
rwehresmann <igoehresmann@gmail.com>
2018-09-09 18:03:33 -0300

    Refact trending topic container
    Now we finally are getting the hashtags returned from the api and storing them in our redux store. Also, I introduced the `constants` file, just to avoid rewrite the actions name, what is very prone for typing error.

--- src/containers/TrendingTopicsContainer/reducer.js
+++ src/containers/TrendingTopicsContainer/reducer.js
@@ -1,9 +1,12 @@
+import { UPDATE_TRENDINGS } from './constants';
+
+// The initial state of the App
 const initialState = { hashtags: [] };
 
 export default function (state = initialState, action) {
   switch (action.type) {
-    case 'UPDATE_TRENDINGS':
-      return { hashtags: [['#OneBitCode', '100k'], ['#RubyOnRails', '10k']] }
+    case UPDATE_TRENDINGS:
+      return { ...state, hashtags: action.payload.hashtags }
     default:
       return state;
   }
