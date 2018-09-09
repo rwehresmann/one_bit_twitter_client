61b703c5cdf29d3c1e1c144f4a6003cc89c81c98
rwehresmann <igoehresmann@gmail.com>
2018-09-09 16:54:08 -0300

    Include axios and call api in trending topics

--- src/containers/TrendingTopicsContainer/index.js
+++ src/containers/TrendingTopicsContainer/index.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
 import TrendingTopics from '../../components/TrendingTopics'
+import axios from 'axios';
 
 class TrendingTopicsContainer extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      hashtags: []
+      hashtags: [],
     };
   }
 
   componentDidMount() {
-    this.setState({
-      hashtags: [['#OneBitCode', '10k'], ['#RubyOnRails', '10k']]
-    })
+    axios.get(process.env.REACT_APP_API + '/trending')
+      .then((response) => {
+        console.log(response.data.hashtags);
+        this.setState(() => {
+          return {
+            hashtags: response.data.hashtags
+          }
+        })
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
-
   render() {
     return (
       <div>
