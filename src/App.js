14fb13ad57cdf6b90f8bffc645c8affb0afff2a8
rwehresmann <igoehresmann@gmail.com>
2018-09-12 22:06:03 -0300

    Implement timeline page mockup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
+
 import HomePageContainer from './containers/HomePageContainer'
 import ProfilePageContainer from './containers/ProfilePageContainer'
+import TimelineContainer from './containers/TimelineContainer'
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        {/* <Link to="/wellcome">Home</Link> <br/>
-     <Link to="/user/1">User 1 profile</Link> */}
         <Switch>
           <Route exact path="/" component={HomePageContainer} />
           <Route exact path="/user/:id" component={ProfilePageContainer} />
+          <Route exact path="/timeline" component={TimelineContainer} />
         </Switch>
       </div>
     );
---
+++ src/components/Timeline/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Row, Col } from 'react-materialize';
+import MainLayout from '../MainLayout'
+
+const Timeline = () => (
+  <MainLayout>
+    <Row>
+      <p>Timeline here</p>
+    </Row>
+  </MainLayout>
+);
+
+export default Timeline;
